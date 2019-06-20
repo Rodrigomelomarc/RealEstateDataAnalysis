6c5dbd1ab5718e7b14c16dbae69de27da5bf3257
Rodrigo de Melo <rodrigomelomarcolino@gmail.com>
2019-06-20 10:52:17 -0300

    Initial commit

---
+++ app/Http/Controllers/ScrapingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScrapingController extends Controller
+{
+    public function retrieveData(Client $client){
+        
+    }
+}
